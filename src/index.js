e0494725290d9650bb5b2a7de4b73be41d718d43
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 15:23:38 -0800

    add dependencies and create store with middlewarE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import promise from 'redux-promise';
+import reducers from './reducers';
+import routes from './config/routes';
 import App from './App';
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={createStoreWithMiddleware(reducers)}>
+  </Provider>
 );
