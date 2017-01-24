6ef4ee928d262222ba9884e19551fa508d55f51a
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-24 10:53:03 -0800

    import movie reducer and combie with form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { MovieReducer } from './MovieReducer';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+  movie: MovieReducer,
   form: formReducer
 });
 
