d4d034a216e0f716079a7e6cd3ad07bd45a03236
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:27:34 -0800

    create movie prompt component

---
+++ src/containers/MoviePrompt.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class MoviePrompt extends Component {
+  render(){
+    return(
+      <div>hellllooooo</div>
+    )
+  }
+}
+
+export default MoviePrompt;
