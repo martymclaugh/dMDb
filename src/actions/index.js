8d3d37c541d813dac8fd19f97c26318d71f46c8a
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-24 10:55:49 -0800

    change request url based on props received

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 
 export const FETCH_OMDB = 'FETCH_OMDB';
 
-const FULL_PLOT = "&plot=full&r=json"
 const OMDB_URL ='http://www.omdbapi.com/?'; //+ i= for id || + t= for title
+const FULL_PLOT = "&plot=full&r=json"
 
 export function fetchOmdb(props) {
-  console.log(props, 'action received')
   const { id, title, year } = props
+  let request
   if (id){
-    const request = axios.get(`${OMDB_URL}i=${id}${FULL_PLOT}`)
+    request = axios.get(`${OMDB_URL}i=${id}${FULL_PLOT}`);
   } else if (title && year){
-    const request = axios.get(`${OMDB_URL}t=${title}&y=${year}${FULL_PLOT}`)
+    request = axios.get(`${OMDB_URL}t=${title}&y=${year}${FULL_PLOT}`);
   } else {
-    const request = axios.get(`${OMDB_URL}t=${title}${FULL_PLOT}`)
+    request = axios.get(`${OMDB_URL}t=${title}${FULL_PLOT}`);
   }
   return {
     type: FETCH_OMDB,
     payload: request
-  }
+  };
 }
