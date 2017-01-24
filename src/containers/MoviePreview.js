052409420d92b71d312b687f041dd895c7c7b754
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-24 11:32:56 -0800

    fetch new data before component is mounted

--- src/containers/MoviePreview.js
+++ src/containers/MoviePreview.js
@@ -1,15 +1,18 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchOmdb } from '../actions';
 
 class MoviePreview extends Component {
   static contextTypes = {
     router: PropTypes.object
   };
+  componentWillMount() {
+    this.props.fetchOmdb(this.props.params.id);
+  }
   render() {
-    console.log(this.context.router)
     return (
       <div>imdb Movie ID: {this.props.params.id}</div>
     )
   }
 }
-
 export default MoviePreview;
