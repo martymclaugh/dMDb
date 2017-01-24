36a5253c5f9ba107af9c9b2948187213f7565d6f
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-24 10:53:53 -0800

    import promise and create store with middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, hashHistory } from 'react-router';
-import promise from 'redux-promise';
 import reducers from './reducers';
 import routes from './config/routes';
+import promise from 'redux-promise';
 import App from './App';
-import './index.css';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
