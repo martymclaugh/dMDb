706ac1aa8bed040679603bb8b956a0f5ea583d9a
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:09:16 -0800

    add child props to main route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Marty</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Main Container
+        {this.props.children}
       </div>
     );
   }
