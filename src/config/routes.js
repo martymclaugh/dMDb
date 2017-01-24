fe765cbd328e38b5eb0f18247ddc3788cc2b104d
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:27:24 -0800

    Add search route

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from '../App';
 import MovieGreeting from '../components/MovieGreeting';
+import MoviePrompt from '../containers/MoviePrompt';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={MovieGreeting} />
+    <Route path="/search" component={MoviePrompt} />
   </Route>
 );
