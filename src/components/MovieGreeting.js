398c99d2eccb5a340d92c46f4f5851325a30d38a
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:20:09 -0800

    change location of component

---
+++ src/components/MovieGreeting.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactRouter, { Link } from 'react-router';
+
+class MovieGreeting extends Component {
+  render(){
+    return (
+      <div className="jumbotron col-sm-12 text-center">
+        <h1>movieD</h1>
+        <p className="lead">Get the rating demographics from iMDB</p>
+        <div className="form-group col-sm-4 col-sm-offset-4">
+          <Link to="/search" className="btn btn-block btn-primary">
+              Continue
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MovieGreeting;
