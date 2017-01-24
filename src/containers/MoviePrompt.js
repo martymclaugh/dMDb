34f76c58ed19cb3ea6752fb3e24914dbdffd1e94
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:34:14 -0800

    add form to movie prompt

--- src/containers/MoviePrompt.js
+++ src/containers/MoviePrompt.js
@@ -4,7 +4,21 @@ import { reduxForm } from 'redux-form';
 class MoviePrompt extends Component {
   render(){
     return(
-      <div>hellllooooo</div>
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <div className="col-sm-12">
+          <form>
+            <h1>Search For A Movie</h1>
+            <div className="form-group">
+              <label> Movie Title: </label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label> Movie Year (optional): </label>
+              <input type="text" className="form-control" />
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }
