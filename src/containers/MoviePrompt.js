dfdc68bb97fe724a9b4449d179a4f1e94578705e
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:52:52 -0800

    start with redux-form functions

--- src/containers/MoviePrompt.js
+++ src/containers/MoviePrompt.js
@@ -3,18 +3,23 @@ import { reduxForm } from 'redux-form';
 
 class MoviePrompt extends Component {
   render(){
+    const { fields: { title, year }} = this.props;
+    console.log(this.props.title)
     return(
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
         <div className="col-sm-12">
           <form>
             <h1>Search For A Movie</h1>
             <div className="form-group">
-              <label> Movie Title: </label>
-              <input type="text" className="form-control" />
+              <label>Movie Title:</label>
+              <input type="text" className="form-control" {...title}/>
             </div>
             <div className="form-group">
-              <label> Movie Year (optional): </label>
-              <input type="text" className="form-control" />
+              <label>Year (optional):</label>
+              <input placeholder="optional" type="text" className="form-control" {...year}/>
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button className="btn btn-block btn-primary" type="submit">Continue</button>
             </div>
           </form>
         </div>
@@ -23,4 +28,8 @@ class MoviePrompt extends Component {
   }
 }
 
-export default MoviePrompt;
+
+export default reduxForm({
+  form: 'MoviePromptForm',
+  fields: ['title', 'year']
+}, null, null)(MoviePrompt);
