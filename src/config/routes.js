bb9507a8028db8602fd7328c06acb3276f326eee
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 16:19:52 -0800

    change directory of import

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from '../App';
-import MovieGreeting from '../containers/MovieGreeting';
+import MovieGreeting from '../components/MovieGreeting';
 
 export default (
   <Route path="/" component={App}>
