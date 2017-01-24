68cf279431694ba23409dc4cbaf1bb58c704a95d
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 18:53:41 -0800

    add url and fetch ombd function

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,21 @@ import axios from 'axios';
 
 export const FETCH_OMDB = 'FETCH_OMDB';
 
-export function fetchOmdb(){
+const FULL_PLOT = "&plot=full&r=json"
+const OMDB_URL ='http://www.omdbapi.com/?'; //+ i= for id || + t= for title
+
+export function fetchOmdb(props) {
+  console.log(props, 'action received')
+  const { id, title, year } = props
+  if (id){
+    const request = axios.get(`${OMDB_URL}i=${id}${FULL_PLOT}`)
+  } else if (title && year){
+    const request = axios.get(`${OMDB_URL}t=${title}&y=${year}${FULL_PLOT}`)
+  } else {
+    const request = axios.get(`${OMDB_URL}t=${title}${FULL_PLOT}`)
+  }
   return {
     type: FETCH_OMDB,
-    payload: 'asdf'
+    payload: request
   }
 }
