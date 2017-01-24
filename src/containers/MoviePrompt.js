f81251854802233a6629c322fe3411df0ce8d7a9
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-23 17:41:30 -0800

    start redux form

--- src/containers/MoviePrompt.js
+++ src/containers/MoviePrompt.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { fetchOmdb } from '../actions/index';
 
 class MoviePrompt extends Component {
   render(){
-    const { fields: { title, year }} = this.props;
-    console.log(this.props.title)
+    const { handleSubmit, fields } = this.props
+    const { title, year } = this.props.fields
     return(
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
         <div className="col-sm-12">
@@ -12,11 +13,25 @@ class MoviePrompt extends Component {
             <h1>Search For A Movie</h1>
             <div className="form-group">
               <label>Movie Title:</label>
-              <input type="text" className="form-control" {...title}/>
+              <input
+                type="text"
+                className="form-control"
+                {...title}/>
+              <div className="text-help">
+                {title.touched ? title.error : ''}
+              </div>
             </div>
             <div className="form-group">
               <label>Year (optional):</label>
-              <input placeholder="optional" type="text" className="form-control" {...year}/>
+              <input
+                maxLength="4"
+                placeholder="optional"
+                type="text"
+                className="form-control"
+                {...year}/>
+                <div className="text-help">
+                  {year.touched ? year.error : ''}
+                </div>
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
               <button className="btn btn-block btn-primary" type="submit">Continue</button>
@@ -28,8 +43,18 @@ class MoviePrompt extends Component {
   }
 }
 
+function validate(values){
+  const errors = {};
+  if(!values.title){
+    errors.title = 'Enter a Title';
+  }
+  if(values.year){
+    errors.title = 'Enter a Year'
+  }
+}
 
 export default reduxForm({
   form: 'MoviePromptForm',
-  fields: ['title', 'year']
-}, null, null)(MoviePrompt);
+  fields: ['title', 'year'],
+  validate
+}, null, fetchOmdb)(MoviePrompt);
