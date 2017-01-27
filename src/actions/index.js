7f2e912270062642d5a76a46d8e27ba2c6fef25f
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-27 14:57:21 -0800

    rewrite actions to fetch omdb, tmdb data, and youtube trailer for selected movie

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,53 +1,59 @@
 import axios from 'axios';
+
+export const FETCH_TMDB = 'FETCH_TMDB';
+export const FETCH_TMDB_ID = 'FETCH_TMDB_ID';
+export const FETCH_OMDB_ID = 'FETCH_OMDB_ID';
+export const FETCH_YOUTUBE_TRAILER = 'FETCH_YOUTUBE_TRAILER';
+
 const YOUTUBE_API_KEY = 'AIzaSyBWyyHS4PatbpxSZAuN3HBfQH1OQLnaj0Y';
 const TMDB_API_KEY = '25a41e10fc0fc533a91edbb4d876705d';
-export const FETCH_OMDB = 'FETCH_OMDB';
-export const FETCH_TMDB = 'FETCH_TMDB';
-
-const OMDB_URL ='http://www.omdbapi.com/?'; //+ i= for id || + t= for title
+const OMDB_URL ='http://www.omdbapi.com/?i='; // i= for id || + t= for title
 const FULL_PLOT = "&plot=full&r=json"
-const TMDB_URL = 'https://api.themoviedb.org/3/search/multi?api_key='
+const TMDB_URL_SEARCH = 'https://api.themoviedb.org/3/search/multi?api_key='
+const TMDB_ID_SEARCH = 'https://api.themoviedb.org/3/movie/'
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key='
 const TMDB_LANGUAGE = '&language=en-US'
 const TMDB_SEARCH_PARAMS = '&page=1&include_adult=false'
+// https://api.themoviedb.org/3/movie/109428?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US id search
+
 // https://api.themoviedb.org/3/search/multi?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US&query=evil%20dead&page=1&include_adult=false // for search
-// export function fetchTmdbSearch(props){
-//   if (props.length > 0){
-//     let searchTerm = props
-//     let devOptions = {
-//       url: `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}`,
-//       dataType: 'jsonp'
-//     }
-//     // const searchTerm = props
-//     // const request = axios.get(`${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`)
-//     const request = axios.get(devOptions)
-//     console.log(request)
-//     return {
-//       type: FETCH_TMDB,
-//       payload: "asdf"
-//     }
-//   }
-// }
-export function fetchOmdbTitle(props) {
-  const { title, year } = props
-  // action for get movie request by title and year(optional)
-  // query still works without year
-  const request = axios.get(`${OMDB_URL}t=${title}&y=${year}${FULL_PLOT}`);
-  return {
-    type: FETCH_OMDB,
-    payload: request
-  };
-}
+
 
 export function fetchTmdbSearch(props) {
   const searchTerm = props
-  const url = `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`;
   const devOptions = {
-    url: `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`
+    url: `${TMDB_URL_SEARCH}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`
   }
-  // action for get movie request by imdb id
   const request = axios(devOptions)
   return {
-    type: FETCH_OMDB,
+    type: FETCH_TMDB,
     payload: request
   };
+}
+export function fetchTmdbId(id){
+  const url = `${TMDB_ID_SEARCH}${id}?api_key=${TMDB_API_KEY}${TMDB_LANGUAGE}`
+  const request = axios(url)
+  return {
+    type: FETCH_TMDB_ID,
+    payload: request
+  }
+}
+export function fetchOmdbId(id){
+  const url = `${OMDB_URL}${id}${FULL_PLOT}`
+  const request = axios(url)
+  return {
+    type: FETCH_OMDB_ID,
+    payload: request
+  }
+}
+export function fetchYoutubeTrailer(query){
+  const { year, title } = query
+  const searchTerm = `${title} ${year}`.split(' ').join('+')
+  const url = `${YOUTUBE_URL}${YOUTUBE_API_KEY}&q=${searchTerm}+trailer&type=video`
+  const request = axios(url)
+  console.log('SENDING ACTION')
+  return {
+    type: FETCH_YOUTUBE_TRAILER,
+    payload: request
+  }
 }
