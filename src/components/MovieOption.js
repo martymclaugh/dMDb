16dcc87787404996705ff9fc4891f0f65c41a31d
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-27 12:37:52 -0800

    add movie option container

---
+++ src/components/MovieOption.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default (props) => {
+  const { poster_path, title, release_date, id } = props
+  return (
+    <div
+      onClick={() => props.onSelectMovie(props)}
+      key={id}
+      className="movie-option-container">
+      <div className="movie-option-details"></div>
+      <img className="movie-option-image" src={`http://image.tmdb.org/t/p/w75${props.poster_path}`} alt={`${title}`}/>
+      <h3 className="movie-option-title lead">{title} ({release_date.slice(0,4)})</h3>
+    </div>
+  )
+}
