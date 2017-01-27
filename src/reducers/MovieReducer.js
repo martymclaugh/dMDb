ec88703eff7f95d4ba8f222bda828c7b6090b28d
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-27 15:02:01 -0800

    Add reducer for omdb, tmdb, and youtube actions

--- src/reducers/MovieReducer.js
+++ src/reducers/MovieReducer.js
@@ -1,12 +1,29 @@
-import { FETCH_OMDB, FETCH_TMDB } from '../actions';
+import {
+  FETCH_TMDB_ID,
+  FETCH_TMDB,
+  FETCH_OMDB_ID,
+  FETCH_YOUTUBE_TRAILER
+} from '../actions';
 
-const INITIAL_STATE = { movies: [], movie: {}, ratings: [] };
+const INITIAL_STATE = {
+  movies: [],
+  tmdbMovie: {},
+  omdbMovie: {},
+  trailer: {},
+  ratings: []
+};
 export const MovieReducer = (state = INITIAL_STATE, action) => {
+  console.log('ACTION RECEIVED: ', action.type)
   switch(action.type) {
   case FETCH_TMDB:
-    return {...state, movie: action.payload.data};
-  case FETCH_OMDB:
     return {...state, movies: action.payload.data.results};
+  case FETCH_TMDB_ID:
+    return {...state, tmdbMovie: action.payload.data}
+  case FETCH_OMDB_ID:
+    return {...state, omdbMovie: action.payload.data}
+  case FETCH_YOUTUBE_TRAILER:
+  console.log(action.payload.data, 'YOUTUBE')
+    return {...state, trailer: action.payload.data }
   default:
     return state;
   }
