a403e1607c23b3ba9497f0a4ac3fe0633b52185b
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-27 12:33:54 -0800

    Add tmdb action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,36 @@
 import axios from 'axios';
-
+const YOUTUBE_API_KEY = 'AIzaSyBWyyHS4PatbpxSZAuN3HBfQH1OQLnaj0Y';
+const TMDB_API_KEY = '25a41e10fc0fc533a91edbb4d876705d';
 export const FETCH_OMDB = 'FETCH_OMDB';
+export const FETCH_TMDB = 'FETCH_TMDB';
 
 const OMDB_URL ='http://www.omdbapi.com/?'; //+ i= for id || + t= for title
 const FULL_PLOT = "&plot=full&r=json"
-
+const TMDB_URL = 'https://api.themoviedb.org/3/search/multi?api_key='
+const TMDB_LANGUAGE = '&language=en-US'
+const TMDB_SEARCH_PARAMS = '&page=1&include_adult=false'
+// https://api.themoviedb.org/3/search/multi?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US&query=evil%20dead&page=1&include_adult=false // for search
+// export function fetchTmdbSearch(props){
+//   if (props.length > 0){
+//     let searchTerm = props
+//     let devOptions = {
+//       url: `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}`,
+//       dataType: 'jsonp'
+//     }
+//     // const searchTerm = props
+//     // const request = axios.get(`${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`)
+//     const request = axios.get(devOptions)
+//     console.log(request)
+//     return {
+//       type: FETCH_TMDB,
+//       payload: "asdf"
+//     }
+//   }
+// }
 export function fetchOmdbTitle(props) {
   const { title, year } = props
-  console.log(props, 'PROPS')
+  // action for get movie request by title and year(optional)
+  // query still works without year
   const request = axios.get(`${OMDB_URL}t=${title}&y=${year}${FULL_PLOT}`);
   return {
     type: FETCH_OMDB,
@@ -15,10 +38,14 @@ export function fetchOmdbTitle(props) {
   };
 }
 
-export function fetchOmdbId(props) {
-  const { id } = props
-  console.log(props, 'ID PROPS')
-  const request = axios.get(`${OMDB_URL}i=${props}${FULL_PLOT}`);
+export function fetchTmdbSearch(props) {
+  const searchTerm = props
+  const url = `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`;
+  const devOptions = {
+    url: `${TMDB_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`
+  }
+  // action for get movie request by imdb id
+  const request = axios(devOptions)
   return {
     type: FETCH_OMDB,
     payload: request
