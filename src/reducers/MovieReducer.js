49fc8d85e45b3078dd8df3621931ecf4fb655210
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-27 12:37:06 -0800

    change reducer to match ajax call

--- src/reducers/MovieReducer.js
+++ src/reducers/MovieReducer.js
@@ -1,10 +1,13 @@
-import { FETCH_OMDB } from '../actions/index';
+import { FETCH_OMDB, FETCH_TMDB } from '../actions';
 
-const INITIAL_STATE = { movie: null, ratings: null };
+const INITIAL_STATE = { movies: [], movie: {}, ratings: [] };
 export const MovieReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
-  case FETCH_OMDB:
+  case FETCH_TMDB:
     return {...state, movie: action.payload.data};
+  case FETCH_OMDB:
+    return {...state, movies: action.payload.data.results};
+  default:
+    return state;
   }
-  return state;
 }
