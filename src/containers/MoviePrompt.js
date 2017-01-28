17ba352c95c3ea141f8106cbbb67fde011014e91
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-28 13:41:09 -0800

    take out unused impot

--- src/containers/MoviePrompt.js
+++ src/containers/MoviePrompt.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
 import _ from 'lodash';
 
 import SearchBar from './SearchBar';
