3ea4fc9707ed19755859ae117f2ad883bd080d4f
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-28 16:45:28 -0800

    add fetch popular action

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ export const FETCH_OMDB_ID = 'FETCH_OMDB_ID';
 export const FETCH_YOUTUBE_TRAILER = 'FETCH_YOUTUBE_TRAILER';
 export const FETCH_IMDB_RATINGS = 'FETCH_IMDB_RATINGS';
 export const FETCH_NOW_PLAYING = 'FETCH_NOW_PLAYING';
+export const FETCH_POPULAR = 'FETCH_POPULAR';
 
 const YOUTUBE_API_KEY = 'AIzaSyBWyyHS4PatbpxSZAuN3HBfQH1OQLnaj0Y';
 const TMDB_API_KEY = '25a41e10fc0fc533a91edbb4d876705d';
@@ -17,7 +18,8 @@ const TMDB_ID_SEARCH = 'https://api.themoviedb.org/3/movie/';
 const TMDB_LANGUAGE = '&language=en-US';
 const TMDB_SEARCH_PARAMS = '&page=1&include_adult=false';
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=';
-const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
+const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
+const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular?api_key='
 // https://api.themoviedb.org/3/movie/109428?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US id search
 
 // https://api.themoviedb.org/3/search/multi?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US&query=evil%20dead&page=1&include_adult=false // for search
@@ -25,9 +27,16 @@ const NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=
 export function fetchNowPlaying(){
   const url = `${NOW_PLAYING_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1&region=US`
   const request = axios(url)
-  console.log(url)
   return {
     type: FETCH_NOW_PLAYING,
+    payload: request
+  }
+}
+export function fetchPopular(){
+  const url = `${POPULAR_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1`
+  const request = axios(url)
+  return {
+    type: FETCH_POPULAR,
     payload: request
   }
 }
