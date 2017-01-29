ee5f10db0202dbe9264e6e210e989654394f05c0
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-29 10:55:21 -0800

    change iframe

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 export default (props) => {
-  console.log(props)
   if(!props.items){
     return <div> loading... </div>
   }
   const video = props.items[0]
   const url = `https://www.youtube.com/embed/${video.id.videoId}`
-  console.log(url)
   return (
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe src={url} frameBorder="0" className="embed-responsive-item"></iframe>
+      <iframe
+        src={url}
+        frameBorder="0"
+        allowFullScreen
+        className="embed-responsive-item"></iframe>
       </div>
   )
 }
