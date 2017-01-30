75a67bd1f58c2bc9f63c94d70e7cca362aa878b7
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-30 08:31:34 -0800

    add footer component

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -5,20 +5,21 @@ import NowPlaying from '../containers/NowPlaying';
 import Popular from '../containers/Popular';
 import TopRated from '../containers/TopRated';
 import Upcoming from '../containers/Upcoming';
-
+import Footer from '../components/Footer';
 
 
 class HomePage extends Component {
   render(){
+    console.log(this.props)
     return(
       <div className="jumbotron">
         <div className="homepage-container">
-
           <NavBar/>
           <NowPlaying />
           <Popular />
           <TopRated />
           <Upcoming />
+          <Footer />
         </div>
       </div>
     )
