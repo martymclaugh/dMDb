c0fe490889df482c39d49a545a545520eb76d50c
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-30 16:05:15 -0800

    change dmdb score calculation

--- src/components/DmdbScore.js
+++ src/components/DmdbScore.js
@@ -4,15 +4,34 @@ import _ from 'lodash';
 export default (props) => {
   const { Metascore, imdbRating, tomatoMeter, tomatoUserMeter } = props.score;
   function calculateDmdb(arr){
-    let temp = arr.map(num => { return parseInt(num, 10)})
-    return ((_.sum(temp) / (arr.length * 1.5)) / 10).toFixed(1)
+    const temp = arr.filter(num => {
+      if(!isNaN(num)){
+        return num
+      }
+    })
+    const scoreArr = temp.map((num) => {return parseInt(num, 10)})
+    if(scoreArr.length === 4){
+      return (_.sum(scoreArr) / (scoreArr.length * 1.5) / 10).toFixed(1)
+    } else {
+      return ((_.sum(scoreArr) / scoreArr.length) / 10).toFixed(1)
+    }
   }
-  const score = calculateDmdb([
-    Metascore,
-    parseFloat(imdbRating) * 20,
-    tomatoMeter,
-    parseInt(tomatoUserMeter, 10) * 2
-  ])
+  let score;
+  if(isNaN(Metascore) || isNaN(tomatoMeter)){
+      score = calculateDmdb([
+      Metascore,
+      parseFloat(imdbRating) * 10,
+      tomatoMeter,
+      parseInt(tomatoUserMeter, 10)
+    ])
+  } else {
+      score = calculateDmdb([
+      Metascore,
+      parseFloat(imdbRating) * 20,
+      tomatoMeter,
+      parseInt(tomatoUserMeter, 10) * 2
+    ])
+  }
   return(
     <div className="score-container">
       <img
