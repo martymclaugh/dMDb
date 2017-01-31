2849a9528c8d7c38bfcd7c85e14ce1c911a3f4ad
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-30 17:18:50 -0800

    add average dashed line, change x and y axis colors, change y axis range

--- src/components/ImdbDemographics.js
+++ src/components/ImdbDemographics.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import _ from 'lodash';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, ReferenceLine, Tooltip, Legend} from 'recharts';
 import Loading from 'react-loading';
 
 
@@ -12,6 +13,10 @@ export default (props) => {
       </div>
     )
   }
+  function calculateAverage(arr){
+    const sum = arr.map(obj => { return obj.males + obj.females})
+    return _.sum(sum) / (arr.length * 2)
+  }
   return (
     <div className="ratings-container">
       <h4 className="movie-details-title">Based on IMDb ratings:</h4>
@@ -19,11 +24,21 @@ export default (props) => {
       width={600}
       height={300}
       data={ratings}
-      margin={{top: 5, right: 30, left: 20, bottom: 25}}>
+      margin={{top: 10, right: 30, left: 20, bottom: 25}}>
       <Legend />
-      <XAxis dataKey="name"/>
-      <YAxis/>
+      <XAxis
+        tick={{stroke: 'white', strokeWidth: 1}}
+        dataKey="name"/>
+      <YAxis
+        tick={{stroke: 'white', strokeWidth: 1}}
+        interval={1}
+        domain={['dataMin - 1', 'dataMax + .5']}/>
       <CartesianGrid strokeDasharray="3 3"/>
+      <ReferenceLine
+        isFront={true}
+        y={calculateAverage(ratings)}
+        strokeDasharray="10 5"
+        stroke="red" />
       <Tooltip/>
       <Bar dataKey="males" fill="#128dea" />
       <Bar dataKey="average" fill="#0D5746" />
