95ef3747c2fa2830b29d37a433b245c12f3b0651
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-04 14:14:23 -0800

    import action types from new file

--- src/reducers/MovieReducer.js
+++ src/reducers/MovieReducer.js
@@ -1,17 +1,4 @@
-import {
-  FETCH_TMDB_ID,
-  FETCH_TMDB,
-  FETCH_OMDB_ID,
-  FETCH_YOUTUBE_TRAILER,
-  FETCH_IMDB_RATINGS,
-  FETCH_NOW_PLAYING,
-  FETCH_POPULAR,
-  FETCH_TOP_RATED,
-  FETCH_UPCOMING,
-  FETCH_SIMILAR,
-  POST_MOVIE_VIEW,
-  FETCH_RECENTLY_VIEWED
-} from '../actions';
+import * as types from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   selectedMovie: {},
@@ -29,27 +16,27 @@ const INITIAL_STATE = {
 };
 export const MovieReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
-  case FETCH_POPULAR:
+  case types.FETCH_POPULAR:
     return {...state, popular: action.payload.data.results};
-  case FETCH_UPCOMING:
+  case types.FETCH_UPCOMING:
     return {...state, upcoming: action.payload.data.results};
-  case FETCH_TOP_RATED:
+  case types.FETCH_TOP_RATED:
     return {...state, topRated: action.payload.data.results};
-  case FETCH_NOW_PLAYING:
+  case types.FETCH_NOW_PLAYING:
     return {...state, nowPlaying: action.payload.data.results};
-  case FETCH_SIMILAR:
+  case types.FETCH_SIMILAR:
     return {...state, similar: action.payload.data.results};
-  case FETCH_TMDB:
+  case types.FETCH_TMDB:
     return {...state, movies: action.payload.data.results};
-  case FETCH_TMDB_ID:
+  case types.FETCH_TMDB_ID:
     return {...state, tmdbMovie: action.payload.data};
-  case FETCH_OMDB_ID:
+  case types.FETCH_OMDB_ID:
     return {...state, omdbMovie: action.payload.data};
-  case FETCH_YOUTUBE_TRAILER:
+  case types.FETCH_YOUTUBE_TRAILER:
     return {...state, trailer: action.payload.data};
-  case FETCH_IMDB_RATINGS:
+  case types.FETCH_IMDB_RATINGS:
     return {...state, ratings: action.payload.data};
-  case FETCH_RECENTLY_VIEWED:
+  case types.FETCH_RECENTLY_VIEWED:
     console.log(action.payload)
     return {...state, recentlyViewed: action.payload}
   default:
