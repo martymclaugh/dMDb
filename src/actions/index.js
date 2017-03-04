52936c665546358b732857d77c12b6dcb93a800d
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-04 14:13:12 -0800

    move action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,5 @@
 import axios from 'axios';
-
-export const FETCH_TMDB = 'FETCH_TMDB';
-export const FETCH_TMDB_ID = 'FETCH_TMDB_ID';
-export const FETCH_OMDB_ID = 'FETCH_OMDB_ID';
-export const FETCH_YOUTUBE_TRAILER = 'FETCH_YOUTUBE_TRAILER';
-export const FETCH_IMDB_RATINGS = 'FETCH_IMDB_RATINGS';
-export const FETCH_NOW_PLAYING = 'FETCH_NOW_PLAYING';
-export const FETCH_POPULAR = 'FETCH_POPULAR';
-export const FETCH_TOP_RATED = 'FETCH_TOP_RATED';
-export const FETCH_UPCOMING = 'FETCH_UPCOMING';
-export const FETCH_SIMILAR = 'FETCH_SIMILAR';
-export const POST_MOVIE_VIEW = 'POST_MOVIE_VIEW';
-export const FETCH_RECENTLY_VIEWED = 'FETCH_RECENTLY_VIEWED';
+import * as types from './actionTypes';
 
 const YOUTUBE_API_KEY = 'AIzaSyBWyyHS4PatbpxSZAuN3HBfQH1OQLnaj0Y';
 const TMDB_API_KEY = '25a41e10fc0fc533a91edbb4d876705d';
@@ -29,15 +17,12 @@ const TOP_RATED_URL ='https://api.themoviedb.org/3/movie/top_rated?api_key=';
 const UPCOMING_URL ='https://api.themoviedb.org/3/movie/upcoming?api_key=';
 const SIMILAR_URL = 'https://api.themoviedb.org/3/movie/';
 const DMDB_API_URL = 'http://localhost:3001/movies';
-// https://api.themoviedb.org/3/movie/109428?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US id search
-
-// https://api.themoviedb.org/3/search/multi?api_key=25a41e10fc0fc533a91edbb4d876705d&language=en-US&query=evil%20dead&page=1&include_adult=false // for search
 
 export function fetchNowPlaying(){
   const url = `${NOW_PLAYING_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1&region=US`
   const request = axios(url)
   return {
-    type: FETCH_NOW_PLAYING,
+    type: types.FETCH_NOW_PLAYING,
     payload: request
   }
 }
@@ -45,7 +30,7 @@ export function fetchPopular(){
   const url = `${POPULAR_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1`
   const request = axios(url)
   return {
-    type: FETCH_POPULAR,
+    type: types.FETCH_POPULAR,
     payload: request
   }
 }
@@ -53,7 +38,7 @@ export function fetchTopRated(){
   const url = `${TOP_RATED_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1`
   const request = axios(url)
   return {
-    type: FETCH_TOP_RATED,
+    type: types.FETCH_TOP_RATED,
     payload: request
   }
 }
@@ -61,21 +46,21 @@ export function fetchUpcoming(){
   const url = `${UPCOMING_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1&region=US`
   const request = axios(url)
   return {
-    type: FETCH_UPCOMING,
+    type: types.FETCH_UPCOMING,
     payload: request
   }
 }
 export function fetchSimilar(id){
   if (id === undefined){
     return {
-      type: null,
+      type: types.null,
       payload: null
     }
   } else {
     const url = `${SIMILAR_URL}${id}/similar?api_key=${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1`
     const request = axios(url)
     return {
-      type: FETCH_SIMILAR,
+      type: types.FETCH_SIMILAR,
       payload: request
     }
   }
@@ -84,14 +69,14 @@ export function fetchTmdbSearch(props) {
   const searchTerm = props;
   if(searchTerm.length === 0){
     return {
-      type: null,
+      type: types.null,
       payload: null
     }
   }
   const url = `${TMDB_URL_SEARCH}${TMDB_API_KEY}${TMDB_LANGUAGE}&query=${searchTerm}${TMDB_SEARCH_PARAMS}`
   const request = axios(url);
   return {
-    type: FETCH_TMDB,
+    type: types.FETCH_TMDB,
     payload: request
   };
 }
@@ -99,7 +84,7 @@ export function fetchTmdbId(id){
   const url = `${TMDB_ID_SEARCH}${id}?api_key=${TMDB_API_KEY}${TMDB_LANGUAGE}`;
   const request = axios(url);
   return {
-    type: FETCH_TMDB_ID,
+    type: types.FETCH_TMDB_ID,
     payload: request
   }
 }
@@ -107,7 +92,7 @@ export function fetchOmdbId(id){
   const url = `${OMDB_URL}${id}${FULL_PLOT}&tomatoes=true`;
   const request = axios(url);
   return {
-    type: FETCH_OMDB_ID,
+    type: types.FETCH_OMDB_ID,
     payload: request
   }
 }
@@ -117,7 +102,7 @@ export function fetchYoutubeTrailer(query){
   const url = `${YOUTUBE_URL}${YOUTUBE_API_KEY}&q=${searchTerm}+trailer&type=video`;
   const request = axios(url);
   return {
-    type: FETCH_YOUTUBE_TRAILER,
+    type: types.FETCH_YOUTUBE_TRAILER,
     payload: request
   }
 }
@@ -125,7 +110,7 @@ export function fetchImdbRatings(imdbId) {
   var url = `${IMDB_SCRAPE_URL}${imdbId}`;
   const request = axios.get(url);
   return {
-    type: FETCH_IMDB_RATINGS,
+    type: types.FETCH_IMDB_RATINGS,
     payload: request
   }
 }
@@ -135,7 +120,7 @@ export function createMovieView(movieData){
     movie: movieData
   });
   return {
-    type: POST_MOVIE_VIEW,
+    type: types.POST_MOVIE_VIEW,
     payload: request
   }
 }
@@ -143,7 +128,7 @@ export function fetchRecentlyViewed(){
   var url = `${DMDB_API_URL}`;
   const request = axios.get(url);
   return {
-    type: FETCH_RECENTLY_VIEWED,
+    type: types.FETCH_RECENTLY_VIEWED,
     payload: request
   }
 }
