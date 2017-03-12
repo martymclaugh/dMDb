7fb739f0a730270eeb79a6bb9ba8e4f75bd2938f
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-11 18:05:55 -0800

    remove console log

--- src/sagas/NowPlayingSaga.js
+++ src/sagas/NowPlayingSaga.js
@@ -4,17 +4,7 @@ import * as types from '../actions/actionTypes';
 import * as Actions from '../actions';
 import * as constants from '../api-helpers/constants';
 
-// export function fetchNowPlaying(){
-//   const url = `${NOW_PLAYING_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1&region=US`
-//   const request = axios(url)
-//   return {
-//     type: types.FETCH_NOW_PLAYING,
-//     payload: request
-//   }
-// }
-
 export function* fetchNowPlaying() {
-  console.log('IN NOW PLAYING SAGA')
   try {
     const url = `${constants.NOW_PLAYING_URL}${constants.TMDB_API_KEY}${constants.TMDB_LANGUAGE}&page=1&region=US`;
     const fetchMovies = () => axios.get(url);
