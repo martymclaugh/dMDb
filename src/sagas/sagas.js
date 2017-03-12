11fd94350a21fbd0c660cb441c702acf47ec2967
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-12 16:12:55 -0700

    Add upcoming saga

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -2,11 +2,13 @@ import { fork } from 'redux-saga/effects';
 import nowPlayingSaga from './NowPlayingSaga';
 import popularSaga from './PopularSaga';
 import topRatedSaga from './TopRatedSaga';
+import upcomingSaga from './UpcomingSaga';
 
 export function* rootSaga(){
   yield [
     fork(nowPlayingSaga),
     fork(popularSaga),
     fork(topRatedSaga),
+    fork(upcomingSaga),
   ]
 }
