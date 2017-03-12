5b1b65f18d6426497535f6d9cf97eda5cf12ce0a
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-11 20:35:24 -0800

    add top rated saga

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,10 +1,12 @@
 import { fork } from 'redux-saga/effects';
 import nowPlayingSaga from './NowPlayingSaga';
 import popularSaga from './PopularSaga';
+import topRatedSaga from './TopRatedSaga';
 
 export function* rootSaga(){
   yield [
     fork(nowPlayingSaga),
     fork(popularSaga),
+    fork(topRatedSaga),
   ]
 }
