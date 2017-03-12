9ebe099eb75f1fffee53a45a81d399c4295edb0d
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-12 15:52:48 -0700

    correct url from error handling test

--- src/sagas/TopRatedSaga.js
+++ src/sagas/TopRatedSaga.js
@@ -4,18 +4,9 @@ import * as types from '../actions/actionTypes';
 import * as Actions from '../actions';
 import * as constants from '../api-helpers/constants';
 
-// export function fetchTopRated(){
-//   const url = `${TOP_RATED_URL}${TMDB_API_KEY}${TMDB_LANGUAGE}&page=1`
-//   const request = axios(url)
-//   return {
-//     type: types.FETCH_TOP_RATED,
-//     payload: request
-//   }
-// }
-
 export function* fetchTopRated() {
   try {
-    const url = `${constants.TOP_RATED_URL}r${constants.TMDB_API_KEY}${constants.TMDB_LANGUAGE}&page=1`;
+    const url = `${constants.TOP_RATED_URL}${constants.TMDB_API_KEY}${constants.TMDB_LANGUAGE}&page=1`;
     const fetchMovies = () => axios.get(url);
     const moviesData = yield call(fetchMovies);
     const movieCollection = moviesData.data;
